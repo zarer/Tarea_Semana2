13c24a34854748999a7759a42ee02162289f7fa6
Brenka <brenka@MacMIni.local>
2016-02-06 21:27:40 -0600

    Initial Commit

---
+++ Memoria Juego Coursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memoria Juego Coursera
+//
+//  Created by Brenka on 06/02/16.
+//  Copyright (c) 2016 ZaraEr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Memoria Juego CourseraTests/Memoria_Juego_CourseraTests.swift
@@ -0,0 +1,36 @@
+//
+//  Memoria_Juego_CourseraTests.swift
+//  Memoria Juego CourseraTests
+//
+//  Created by Brenka on 06/02/16.
+//  Copyright (c) 2016 ZaraEr. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Memoria_Juego_CourseraTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
